3ed54487d684c1dd7e00e1274aef623061659efa
jonathan <jvhuaiying@outlook.com>
2022-07-12 11:47:57 +0800

    1.0.0

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laurence
+{
+    class Class1
+    {
+        public List<string> c01 = new List<string>();
+        public void c02(string d01)
+        {
+            using (StreamReader d02 = new StreamReader(d01, Encoding.UTF8))
+            {
+                while (d02.Peek() != -1)
+                {
+                    string d001 = d02.ReadLine().Trim();
+                    if (d001.Contains("<img"))
+                    {
+                        int d002 = d001.IndexOf("src=", 0);
+                        int d003 = d001.IndexOf('>', d002 + 1);
+                        string d004 = d001.Substring(d002, d003 - d002);
+                        int d005 = d004.IndexOf("\"");
+                        int d006 = d004.IndexOf("\"", d005 + 1);
+                        string d007 = d004.Substring(d005 + 1, d006 - d005 - 1);
+                        c01.Add(d007);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laurence
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void a01(object sender, EventArgs e)
+        {
+            OpenFileDialog b01 = new OpenFileDialog();
+            b01.InitialDirectory = Application.StartupPath;
+            b01.Title = "请选择要打开的文件";
+            b01.Filter = "(网页文件*.html)|*.html";
+            b01.FilterIndex = 1;
+            b01.Multiselect = true;
+            Class1 a001 = new Class1();
+            if (b01.ShowDialog() == DialogResult.OK)
+            {
+                string[] b02 = b01.FileNames;
+                for (int i = 0; i < b02.Length; i++)
+                {
+                    if (textBox1.Text.Contains(b02[i]) != true)
+                    {
+                        textBox1.Text += b02[i] + System.Environment.NewLine;
+                        a001.c02(b02[i]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("文件名已存在，请确认文件或修改文件名！", "文件名已存在");
+                        continue;
+                    }
+                    for (int i01 = 0; i01 < a001.c01.Count; i01++)
+                    {
+                        if (i01 != a001.c01.Count - 1)
+                        {
+                            textBox3.Text += a001.c01[i01] + System.Environment.NewLine;
+                        }
+                        else
+                        {
+                            textBox3.Text += a001.c01[i01];
+                        }
+                    }
+                }
+            }
+        }
+
+        private void a02(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(textBox3.Text, true);
+            MessageBox.Show("图片链接复制完成，感谢您的使用！", "操作成功");
+        }
+    }
+}
